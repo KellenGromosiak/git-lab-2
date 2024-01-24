be766c0b70c213bd7991aaf47e39a15e5a2e9169
KellenGromosiak <kg630622@ohio.edu>
2024-01-23 20:28:09 -0500

    h

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+* File: git-lab_program.cc
+* Author: Kellen Gromosiak
+*Date 1/23/23
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello git!!" << endl;
+    return 0;
+}
